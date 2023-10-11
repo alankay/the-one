c4fc05bf58840b2cebc484cdfa9f7ad77911b376
Alan Kay <alankaydirect@gmail.com>
2023-10-11 23:45:47 +0100

    Moved location of JS script

--- js/checkguess.js
+++ js/checkguess.js
@@ -11,8 +11,8 @@ const checkNumber = () => {
   const userGuess = parseInt(document.getElementById("userGuess").value);
   if (userGuess == randomNumber) {
     // If the user guess is correct, display a success message and update UI elements
-    guessStatus.innerHTML = "<strong>You are the one</strong>, because your guess of  " + randomNumber + " is the mystery number.<br />Take the red pill and let's see how deep the rabbit goes...";
-    document.getElementById("morpheus").src = "img/morph2.png";
+    guessStatus.innerHTML = "<strong>You are the one</strong>, your guess of  " + randomNumber + " is the mystery number.<br />Take the red pill and let's see how deep the rabbit goes...";
+    document.getElementById("morpheus").src = "../img/morph2.png";
     document.getElementById("guessStatus").className = "correct";
     document.getElementById("oneStatus").innerHTML = "You are the one.";
     document.getElementById("oneStatusRule").innerHTML = "Let's save Zion.";
@@ -20,7 +20,7 @@ const checkNumber = () => {
   } else if (userGuess > randomNumber && userGuess < 100) {
     // If the guess is too high, display a message suggesting a lower guess
     guessStatus.innerHTML = "You guessed " + userGuess + " - which is too high. <strong>Take the blue pill</strong>. Ignorance is bliss.<br />Try again if you still believe you could be the one.";
-    document.getElementById("morpheus").src = "img/morph1.png";
+    document.getElementById("morpheus").src = "../img/morph1.png";
     document.getElementById("guessStatus").className = "tooHigh";
   } else if (userGuess < 1 || userGuess > 100) {
     // If the guess is out of the valid range, display an error message
@@ -33,7 +33,7 @@ const checkNumber = () => {
   } else {
     // If the guess is too low, display a message suggesting a higher guess
     guessStatus.innerHTML = "You guessed " + userGuess + " - which is too low. <strong>Take the blue pill</strong>. Ignorance is bliss.<br />Try again if you still believe you could be the one.";
-    document.getElementById("morpheus").src = "img/morph1.png";
+    document.getElementById("morpheus").src = "../img/morph1.png";
     document.getElementById("guessStatus").className = "tooLow";
   }
 };
